a0ef2bf5ddf1cdae4534bc24f5c46aac89040d0c
AnhGam <guardalex8@gmail.com>
2026-03-24 14:04:35 +0700

    feat: implement Mario movement and camera test (pre-refactor)

--- engine/core/GameLoop.cpp
+++ engine/core/GameLoop.cpp
@@ -2,6 +2,8 @@
 #include "Game.h"
 #include "../input/KeyboardManager.h"
 #include "../input/InputManager.h"
+#include "../rendering/Textures.h"
+#include "../rendering/Camera.h"
 #include "../utils/debug.h"
 
 #define BACKGROUND_COLOR D3DXCOLOR(200.0f/255, 200.0f/255, 255.0f/255, 1.0f)
@@ -27,41 +29,113 @@ void HandleDebugInput() {
 #endif
 }
 
+// --- Player state ---
+float marioX = 300.0f;
+float marioY = 100.0f;
+const float MARIO_SPEED = 0.2f;
+
+void UpdateInput()
+{
+    InputManager::GetInstance()->Update();
+    KeyboardManager::GetInstance()->Update();
+    HandleDebugInput();
+}
+
+void UpdatePlayer(DWORD dt)
+{
+    auto state = InputManager::GetInstance()->GetState();
+    if (state.left) marioX -= MARIO_SPEED * dt;
+    if (state.right) marioX += MARIO_SPEED * dt;
+}
+
+void UpdateCamera(DWORD dt)
+{
+    CCamera::GetInstance()->Update(marioX, marioY, dt);
+}
+
 void Update(DWORD dt)
 {
-<<<<<<< Updated upstream
-    InputManager::GetInstance()->Update();
+    UpdateInput();
+    UpdatePlayer(dt);
+    UpdateCamera(dt);
+}
 
-	HandleDebugInput();
+void LoadAssets()
+{
+    static bool assetsLoaded = false;
+    if (assetsLoaded) return;
 
-    KeyboardManager::GetInstance()->Update(); 
-=======
-	// TODO: Handle updates for game objects / scene management
-	// Example: CCamera::GetInstance()->Update(player->x, player->y, dt);
-	CCamera::GetInstance()->Update(0, 0, dt); // Default target for now
->>>>>>> Stashed changes
+    CTextures::GetInstance()->Add(1, L"content/textures/mario.png");
+    CTextures::GetInstance()->Add(2, L"content/textures/misc.png");
+    assetsLoaded = true;
+}
+
+void RenderBackground(CGame* g, ID3D10RenderTargetView* pRenderTargetView, ID3D10Device* pD3DDevice)
+{
+    pD3DDevice->ClearRenderTargetView(pRenderTargetView, BACKGROUND_COLOR);
+}
+
+void RenderPlayer(CGame* g)
+{
+    LPTEXTURE texMario = CTextures::GetInstance()->Get(1);
+    float camX, camY;
+    CCamera::GetInstance()->GetCamPos(camX, camY);
+
+    // CSS: -123px 0; width: 6px; height: 8px;
+    RECT rect;
+    rect.left = 123;
+    rect.top = 0;
+    rect.right = 129;
+    rect.bottom = 8;
+
+    // Draw scale: 4x (24x32)
+    g->Draw(marioX - camX, marioY - camY, texMario, &rect, 1.0f, 24, 32);
+}
+
+void RenderEnvironment(CGame* g)
+{
+    LPTEXTURE texMisc = CTextures::GetInstance()->Get(2);
+    float camX, camY;
+    CCamera::GetInstance()->GetCamPos(camX, camY);
+
+    // Block CSS: -150 -58; Assume 16x16
+    RECT rect;
+    rect.left = 150;
+    rect.top = 58;
+    rect.right = 166;
+    rect.bottom = 74;
+
+    // Draw a row of blocks for camera testing
+    for (int i = 0; i < 20; i++)
+    {
+        float blockX = i * 48.0f; // 16x3 = 48
+        float blockY = 50.0f;
+        g->Draw(blockX - camX, blockY - camY, texMisc, &rect, 1.0f, 48, 48);
+    }
 }
 
 void Render()
 {
-	CGame* g = CGame::GetInstance();
+    CGame* g = CGame::GetInstance();
+    LoadAssets();
 
-	ID3D10Device* pD3DDevice = g->GetDirect3DDevice();
-	IDXGISwapChain* pSwapChain = g->GetSwapChain();
-	ID3D10RenderTargetView* pRenderTargetView = g->GetRenderTargetView();
-	ID3DX10Sprite* spriteHandler = g->GetSpriteHandler();
+    ID3D10Device* pD3DDevice = g->GetDirect3DDevice();
+    IDXGISwapChain* pSwapChain = g->GetSwapChain();
+    ID3D10RenderTargetView* pRenderTargetView = g->GetRenderTargetView();
+    ID3DX10Sprite* spriteHandler = g->GetSpriteHandler();
 
-	pD3DDevice->ClearRenderTargetView(pRenderTargetView, BACKGROUND_COLOR);
+    RenderBackground(g, pRenderTargetView, pD3DDevice);
 
-	spriteHandler->Begin(D3DX10_SPRITE_SORT_TEXTURE);
+    spriteHandler->Begin(D3DX10_SPRITE_SORT_TEXTURE);
 
-	FLOAT NewBlendFactor[4] = { 0,0,0,0 };
-	pD3DDevice->OMSetBlendState(g->GetAlphaBlending(), NewBlendFactor, 0xffffffff);
+    FLOAT NewBlendFactor[4] = { 0,0,0,0 };
+    pD3DDevice->OMSetBlendState(g->GetAlphaBlending(), NewBlendFactor, 0xffffffff);
 
-	// TODO: Draw logic here! e.g., sprites, tiles...
+    RenderEnvironment(g);
+    RenderPlayer(g);
 
-	spriteHandler->End();
-	pSwapChain->Present(0, 0);
+    spriteHandler->End();
+    pSwapChain->Present(0, 0);
 }
 
 int Run()
